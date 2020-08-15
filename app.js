91c9b842f4e49475c30ca7b64379b9576435fb43
mateusz <dalvan234@gmail.com>
2020-08-15 15:42:52 +0200

    calendar showing/hiding, selecting months and days functionality added

--- app.js
+++ app.js
@@ -11,7 +11,56 @@ app.config(['$routeProvider', ($routeProvider) => {
 }])
 
 app.controller('app-controller', ['$scope', '$http', ($scope, $http) => {
-  
+  $scope.calendarVisible = false;
+  $scope.showCalendar = () => {
+    $scope.calendarVisible = !$scope.calendarVisible;
+  }
+}])
+
+app.constant("moment", moment);
+
+app.controller('calendar-controller', ['$scope', '$http', 'moment', ($scope, $http, moment) => {
+  $scope.date1 = moment().format('ll');
+  $scope.date2 = moment().add(1, 'months').format('ll');
+  $scope.year1 = $scope.date1.substr(8, 4);
+  $scope.month1 = $scope.date1.substr(0, 3);
+  $scope.month2 = $scope.date2.substr(0, 3);
+  $scope.year2 = $scope.date2.substr(8, 4);
+  $scope.monthOperation = (value) => {
+    if (value === '-') {
+      $scope.date1 = moment($scope.date1).subtract(1, 'months').format('ll');
+      $scope.date2 = moment($scope.date2).subtract(1, 'months').format('ll');
+      $scope.year1 = $scope.date1.substr(8, 4);
+      $scope.month1 = $scope.date1.substr(0, 3);
+      $scope.month2 = $scope.date2.substr(0, 3);
+      $scope.year2 = $scope.date2.substr(8, 4);
+    } else if (value === "+") {
+      $scope.date1 = moment($scope.date1).add(1, 'months').format('ll');
+      $scope.date2 = moment($scope.date2).add(1, 'months').format('ll');
+      $scope.year1 = $scope.date1.substr(8, 4);
+      $scope.month1 = $scope.date1.substr(0, 3);
+      $scope.month2 = $scope.date2.substr(0, 3);
+      $scope.year2 = $scope.date2.substr(8, 4);
+    }
+  }
+  $scope.chooseDay = (value,num) => {
+    if(num===1){
+      console.log($scope.month1+" "+value+", "+$scope.year1);
+    }else if(num===2){
+      console.log($scope.month2+" "+value+", "+$scope.year2);
+    }
+  }
+  console.log($scope.month1);
+  console.log($scope.month2);
+  console.log($scope.year1);
+  console.log($scope.year2);
+  $scope.checkIn = "Check In";
+  $scope.checkOut = "Check Out";
+  $scope.monthsDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  $scope.num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  $scope.showCalendar = () => {
+    $scope.calendarVisible = !$scope.calendarVisible;
+  }
 }])
 
 
